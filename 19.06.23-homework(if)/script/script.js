01f1a441c8464488c4e46cc86f30b1ed09f902d8
TACHA1986 <126571508+TACHA1986@users.noreply.github.com>
2023-06-23 22:29:46 +0200

    Homework 19.06.23.
    Задание 1: Пользователь вводит число, проверьте его на четность и выведите соответствующее сообщение (if)
    Задание 2: Пользователь вводит число, выведите сообщение, является ли число положительным или отрицательным (тернарный оператор)
    Задание 3: Пользователь вводит число, проверьте его на четность и выведите соответствующее сообщение с помощью тернарного оператора

---
+++ 19.06.23-homework(if)/script/script.js
@@ -0,0 +1,26 @@
+// Задание 1: Пользователь вводит число, проверьте его на четность и выведите соответствующее сообщение (if)
+
+const num = +prompt ("Please, enter your number.")
+
+if (num % 2 === 0) {
+    console.log ("Your number is even.")
+} else {
+    console.log ("Your number is odd.")
+}
+
+
+// ---------------------------------------------------------------------------------
+// Задание 2: Пользователь вводит число, выведите сообщение, является ли число положительным или отрицательным (тернарный оператор).
+
+// const num = +prompt ("Please, enter your number.")
+
+//     num > 0 ? console.log ("Your number is positive.") : 
+//     num === 0 ? console.log("The number 0 is neither positive nor negative.") : console.log("Your number is negative.")
+
+
+//----------------------------------------------------------------------------------
+// Задание 3: Пользователь вводит число, проверьте его на четность и выведите соответствующее сообщение с помощью тернарного оператора.
+
+// const num = +prompt ("Please, enter, your number.")
+
+// num % 2 === 0 ? console.log("Your number is even") : console.log ("Your number is odd.")
